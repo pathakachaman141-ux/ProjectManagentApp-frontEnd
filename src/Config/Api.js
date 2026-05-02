ca64471119a14ac6243219f23fbba1a2c3411bc9
pathakachaman141-ux <pathakachaman141@gmail.com>
2026-05-02 16:27:24 +0530

    Update Api.js

--- src/Config/Api.js
+++ src/Config/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios"; 
-export const API_BASE_URL = "https://project-mgmt-backend-production-b1d3.up.railway.app/";
+export const API_BASE_URL = "https://project-mgmt-backend-production-b1d3.up.railway.app";
  //export const API_BASE_URL = "http://localhost:8080";
 
 const api = axios.create({ baseURL: API_BASE_URL });
